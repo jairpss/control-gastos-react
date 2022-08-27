e886b090aefd14f8410eb649db9ec9161b51bd20
Jair Partida <jairpartida1@gmail.com>
2022-08-27 13:08:40 -0600

    creating header and new budget components, alert component

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import Mensaje from './Mensaje'
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+  
+  const [mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+
+    if(!Number(presupuesto) || Number(presupuesto) < 0) {
+      setMensaje('No es un presupuesto valido')
+    } else {
+      console.log('Si es un presupuesto valido')
+    }
+  }
+  
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+        <form onSubmit={handlePresupuesto} className="formulario">
+            <div className="campo">
+                <label>Definir presupuesto</label>
+                <input 
+                  className="nuevo-presupuesto"
+                  type="text"
+                  placeholder="¿Cual tu presupuesto?"
+                  value={presupuesto}
+                  onChange={ (e) => setPresupuesto(e.target.value)}
+                />
+            </div>
+            <input type="submit" value="Añadir"/>
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
