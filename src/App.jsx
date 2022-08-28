c28f495de4842a69099824a86644c894f8500e56
Jair Partida <jairpartida1@gmail.com>
2022-08-28 13:45:46 -0600

    creating Contro Presupuesto component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import Header from './components/Header'
 
 function App() {
-  const [presupuesto, setPresupuesto] = useState(0)
+  const [presupuesto, setPresupuesto] = useState('')
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+
   return (
     <div>
       <Header 
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
       />
     </div>
   )
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
 import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto, setIsValidPresupuesto}) => {
   
   const [mensaje, setMensaje] = useState('')
 
   const handlePresupuesto = (e) => {
     e.preventDefault();
 
-    if(!Number(presupuesto) || Number(presupuesto) < 0) {
+    if(!presupuesto || presupuesto < 0) {
       setMensaje('No es un presupuesto valido')
-    } else {
-      console.log('Si es un presupuesto valido')
+      return
     }
+    setMensaje('')
+    setIsValidPresupuesto(true)
   }
   
   return (
@@ -22,10 +23,10 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
                 <label>Definir presupuesto</label>
                 <input 
                   className="nuevo-presupuesto"
-                  type="text"
+                  type="number"
                   placeholder="¿Cual tu presupuesto?"
                   value={presupuesto}
-                  onChange={ (e) => setPresupuesto(e.target.value)}
+                  onChange={ (e) => setPresupuesto(Number(e.target.value))}
                 />
             </div>
             <input type="submit" value="Añadir"/>
