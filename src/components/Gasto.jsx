7ace8ffdcb04f2e1bc49ad854f7937e38289c90f
Jair Partida <jairpartida1@gmail.com>
2022-08-30 20:48:53 -0600

    Gasto component done, add gasto logic done

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,5 +1,20 @@
 import React from 'react'
 import { formatearFecha } from '../helpers'
+import IconoAhorro from '../img/icono_ahorro.svg'
+import IconoCasa from '../img/icono_casa.svg'
+import IconoComida from '../img/icono_comida.svg'
+import IconoGastos from '../img/icono_gastos.svg'
+import IconoSalud from '../img/icono_salud.svg'
+import IconoSuscripciones from '../img/icono_suscripciones.svg'
+
+const iconos = {
+    ahorro: IconoAhorro,
+    comida: IconoComida,
+    casa: IconoCasa,
+    gastos: IconoGastos,
+    salud: IconoSalud,
+    suscripciones: IconoSuscripciones
+}
 
 const Gasto = ({gasto}) => {
   const { categoria, nombre, cantidad, id, fecha } = gasto  
@@ -7,6 +22,10 @@ const Gasto = ({gasto}) => {
   return (
     <div className="gasto sombra">
        <div className="contenido-gasto">
+            <img 
+                src={iconos[categoria]}
+                alt="icono gasto"
+            />
             <div className="descripcion-gasto">
                 <p className="categoria">{categoria}</p>
                 <p className="nombre-gasto">{nombre}</p>
